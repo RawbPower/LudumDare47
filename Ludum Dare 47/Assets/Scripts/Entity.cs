5a2d50abac69717804255de63333bba1243a5ac6
Rob Power <robjamespower@gmail.com>
2020-10-04 13:57:06 +0100

    Full Game Loop (You can win now)

--- Ludum Dare 47/Assets/Scripts/Entity.cs
+++ Ludum Dare 47/Assets/Scripts/Entity.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Entity : MonoBehaviour
 {
@@ -40,6 +41,8 @@ public class Entity : MonoBehaviour
     public float freezeTime;
     private float freezeTimer;
     private bool freeze;
+    private bool win;
+    public GameObject winText;
 
     public ResetTransform resetTransform;
 
@@ -51,6 +54,7 @@ public class Entity : MonoBehaviour
 
     private void Awake()
     {
+        win = false;
         resetTransform = new ResetTransform(transform);
         rb = GetComponent<Rigidbody2D>();
         onPlatform = false;
@@ -68,116 +72,142 @@ public class Entity : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        ground = null;
-        isGrounded = false;
-        Collider2D tempGround;
-        for (int i = 0; i < feetPos.Length; i++)
-        { 
-            tempGround = Physics2D.OverlapCircle(feetPos[i].position, checkRadius, whatIsGround);
-            isGrounded = isGrounded || tempGround;
-            if (tempGround)
-            {
-                ground = tempGround;
-            }
-        }
-        isGrounded = ground;
-
-        if (isGrounded)
+        if (!win)
         {
-            ring = ground.gameObject.GetComponent<Ring>();
-            if (ring == null)
+            ground = null;
+            isGrounded = false;
+            Collider2D tempGround;
+            for (int i = 0; i < feetPos.Length; i++)
             {
-                ring = ground.gameObject.GetComponent<FreezeBlocks>().parentTransform.gameObject.GetComponent<Ring>();
+                tempGround = Physics2D.OverlapCircle(feetPos[i].position, checkRadius, whatIsGround);
+                isGrounded = isGrounded || tempGround;
+                if (tempGround)
+                {
+                    ground = tempGround;
+                }
+            }
+            isGrounded = ground;
+
+            if (isGrounded)
+            {
+                ring = ground.gameObject.GetComponent<Ring>();
                 if (ring == null)
                 {
-                    Debug.LogError("Error with rings!");
+                    ring = ground.gameObject.GetComponent<FreezeBlocks>().parentTransform.gameObject.GetComponent<Ring>();
+                    if (ring == null)
+                    {
+                        Debug.LogError("Error with rings!");
+                    }
                 }
             }
-        }
 
-        float move = Input.GetAxis("Horizontal");
-    
-        angle = Mathf.Atan2(transform.position.y, transform.position.x);
-        radius = Mathf.Sqrt(transform.position.x * transform.position.x + transform.position.y * transform.position.y);
+            float move = Input.GetAxis("Horizontal");
 
-        acceleration = gravity;
+            angle = Mathf.Atan2(transform.position.y, transform.position.x);
+            radius = Mathf.Sqrt(transform.position.x * transform.position.x + transform.position.y * transform.position.y);
 
-        if (Input.GetKey(KeyCode.Space) && isGrounded)
-        {
-            Debug.Log("Jump");
-            acceleration -= jump;
-        }
+            acceleration = gravity;
 
-        velocity += acceleration * Time.deltaTime;
-
-        GetComponent<SpriteRenderer>().flipX = (angleVelocity < 0.0f);
-
-        if (onPlatform)
-        {
-            acceleration = 0.0f;
-            velocity = 0.0f;
-        }
-
-        if (Mathf.Abs(velocity) > maxSpeed)
-        {
-            velocity = Mathf.Sign(velocity)*maxSpeed;
-        }
-
-        angleVelocity = move * moveSpeed * Time.deltaTime;
-
-        if (onPlatform && angleVelocity == 0.0f)
-        {
-            if (ring)
+            if (Input.GetKey(KeyCode.Space) && isGrounded)
             {
-                if (!platform.GetComponent<FreezeBlocks>())
+                Debug.Log("Jump");
+                acceleration -= jump;
+            }
+
+            velocity += acceleration * Time.deltaTime;
+
+            GetComponent<SpriteRenderer>().flipX = (angleVelocity < 0.0f);
+
+            if (onPlatform)
+            {
+                acceleration = 0.0f;
+                velocity = 0.0f;
+            }
+
+            if (Mathf.Abs(velocity) > maxSpeed)
+            {
+                velocity = Mathf.Sign(velocity) * maxSpeed;
+            }
+
+            angleVelocity = move * moveSpeed * Time.deltaTime;
+
+            if (onPlatform && angleVelocity == 0.0f)
+            {
+                if (ring)
                 {
-                    angleVelocity = (ring.rotation / ring.radius) * Mathf.Deg2Rad + Mathf.Sign(ring.rotation) * offset;
-                }
-                else if (platform.GetComponent<FreezeBlocks>() && !platform.GetComponent<FreezeBlocks>().GetIsFrozen())
-                {
-                    angleVelocity = (ring.rotation / ring.radius) * Mathf.Deg2Rad + Mathf.Sign(ring.rotation) * offset;
+                    if (!platform.GetComponent<FreezeBlocks>())
+                    {
+                        angleVelocity = (ring.rotation / ring.radius) * Mathf.Deg2Rad + Mathf.Sign(ring.rotation) * offset;
+                    }
+                    else if (platform.GetComponent<FreezeBlocks>() && !platform.GetComponent<FreezeBlocks>().GetIsFrozen())
+                    {
+                        angleVelocity = (ring.rotation / ring.radius) * Mathf.Deg2Rad + Mathf.Sign(ring.rotation) * offset;
+                    }
                 }
             }
+
+            radius += velocity * Time.deltaTime;
+            angle += angleVelocity * Time.deltaTime;
+
+            rotation = angle * Mathf.Rad2Deg + 90.0f;
+
+
+            rb.MovePosition(new Vector2(radius * Mathf.Cos(angle), radius * Mathf.Sin(angle)));
+            //rb.MoveRotation(rotation);
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotation);
         }
+        else
+        {
+            if (freezeTimer < 3.0f)
+            {
+                rb.MovePosition(new Vector2(0.0f, 0.0f));
 
-        radius += velocity * Time.deltaTime;
-        angle += angleVelocity * Time.deltaTime;
+                angleVelocity = 50 * moveSpeed * Time.deltaTime;
+                angle += angleVelocity * Time.deltaTime;
+                rotation = angle * Mathf.Rad2Deg + 90.0f;
+                transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotation);
+                freezeTimer += Time.deltaTime;
+            }
+            else
+            {
+                GetComponent<SpriteRenderer>().enabled = false;
+            }
+          
 
-        rotation = angle * Mathf.Rad2Deg + 90.0f;
-
-
-        rb.MovePosition(new Vector2(radius * Mathf.Cos(angle), radius * Mathf.Sin(angle)));
-        //rb.MoveRotation(rotation);
-        transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotation);
+            winText.GetComponent<Text>().enabled = true;
+        }
 
     }
 
     private void Update()
     {
-       Collider2D hit = Physics2D.OverlapCircle(scan.position, scanRadius, whatIsGround);
-
-        if (Input.GetKeyDown(KeyCode.X))
+        if (!win)
         {
-            freezeTimer = 0.0f;
-            freeze = true;
-            if (hit)
+            Collider2D hit = Physics2D.OverlapCircle(scan.position, scanRadius, whatIsGround);
+
+            if (Input.GetKeyDown(KeyCode.X))
             {
-                if (hit.gameObject.CompareTag("Freezable") && !hit.gameObject.GetComponent<FreezeBlocks>().GetIsFrozen())
+                freezeTimer = 0.0f;
+                freeze = true;
+                if (hit)
                 {
-                    hit.gameObject.GetComponent<FreezeBlocks>().SetIsFrozen(true);
+                    if (hit.gameObject.CompareTag("Freezable") && !hit.gameObject.GetComponent<FreezeBlocks>().GetIsFrozen())
+                    {
+                        hit.gameObject.GetComponent<FreezeBlocks>().SetIsFrozen(true);
+                    }
                 }
             }
-        }
 
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            freezeTimer = 0.0f;
-            freeze = false;
-            if (hit)
+            if (Input.GetKeyDown(KeyCode.Z))
             {
-                if (hit.gameObject.CompareTag("Freezable") && hit.gameObject.GetComponent<FreezeBlocks>().GetIsFrozen())
+                freezeTimer = 0.0f;
+                freeze = false;
+                if (hit)
                 {
-                    hit.gameObject.GetComponent<FreezeBlocks>().SetIsFrozen(false);
+                    if (hit.gameObject.CompareTag("Freezable") && hit.gameObject.GetComponent<FreezeBlocks>().GetIsFrozen())
+                    {
+                        hit.gameObject.GetComponent<FreezeBlocks>().SetIsFrozen(false);
+                    }
                 }
             }
         }
@@ -226,34 +256,42 @@ public class Entity : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log(collision.gameObject.name);
-        collision.gameObject.GetComponent<EdgeCollider2D>().enabled = true;
-        collision.gameObject.GetComponent<SpriteRenderer>().color = new Color(0.05490196f, 0.09803922f, 0.8784314f, 1.0f);
-        BoxCollider2D[] colliders = collision.gameObject.GetComponentsInChildren<BoxCollider2D>();
-
-        foreach (BoxCollider2D bc in colliders)
+        if (collision.gameObject.CompareTag("Ring"))
         {
-            bc.gameObject.GetComponent<SpriteRenderer>().color = new Color(0.05490196f, 0.5950408f, 0.8784314f, 1.0f);
-            bc.enabled = true;
-        }
+            Debug.Log(collision.gameObject.name);
+            collision.gameObject.GetComponent<EdgeCollider2D>().enabled = true;
+            collision.gameObject.GetComponent<SpriteRenderer>().color = new Color(0.05490196f, 0.09803922f, 0.8784314f, 1.0f);
+            BoxCollider2D[] colliders = collision.gameObject.GetComponentsInChildren<BoxCollider2D>();
 
-        Camera camera = Camera.main;
-        if (camera != null)
+            foreach (BoxCollider2D bc in colliders)
+            {
+                bc.gameObject.GetComponent<SpriteRenderer>().color = new Color(0.05490196f, 0.5950408f, 0.8784314f, 1.0f);
+                bc.enabled = true;
+            }
+
+            Camera camera = Camera.main;
+            if (camera != null)
+            {
+                var brain = (camera == null) ? null : camera.GetComponent<CinemachineBrain>();
+                var vcam = (brain == null) ? null : brain.ActiveVirtualCamera as CinemachineVirtualCamera;
+                if (vcam != null)
+                    camera.GetComponent<CamMovement>().SetDesiredOrthographicSize(6.0f * collision.gameObject.GetComponent<Ring>().radius);
+            }
+
+            resetTransform.position.y += 5.0f;
+
+            collision.gameObject.GetComponent<CircleCollider2D>().enabled = false;
+
+            collision.gameObject.GetComponent<Ring>().player = transform;
+
+            timer.GetComponent<LoopClock>().level = collision.gameObject.GetComponent<Ring>();
+
+            collision.gameObject.GetComponent<Ring>().ResetRing(timer.GetComponent<LoopClock>());
+        }
+        else if (collision.gameObject.CompareTag("Portal"))
         {
-            var brain = (camera == null) ? null : camera.GetComponent<CinemachineBrain>();
-            var vcam = (brain == null) ? null : brain.ActiveVirtualCamera as CinemachineVirtualCamera;
-            if (vcam != null)
-                camera.GetComponent<CamMovement>().SetDesiredOrthographicSize(vcam.m_Lens.OrthographicSize / 2.0f);
+            freezeTimer = 0.0f;
+            win = true;
         }
-
-        resetTransform.position.y += 5.0f;
-
-        collision.gameObject.GetComponent<CircleCollider2D>().enabled = false;
-
-        collision.gameObject.GetComponent<Ring>().player = transform;
-
-        timer.GetComponent<LoopClock>().level = collision.gameObject.GetComponent<Ring>();
-
-        collision.gameObject.GetComponent<Ring>().ResetRing(timer.GetComponent<LoopClock>());
     }
 }
